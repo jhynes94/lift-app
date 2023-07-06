b14b6f034e6cac6a00f52050081367993d80ec4f
Justin HB <jbruell94@gmail.com>
2023-07-05 20:01:42 -0700

    Added Axios and Programs API

--- react-frontend/src/components/ProgramsList.js
+++ react-frontend/src/components/ProgramsList.js
@@ -1,8 +1,30 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import benchPress from '../assets/BenchPressDude.gif'
+import React, { useEffect, useState } from "react";
+import axios from 'axios'
+
+// reference https://dev.to/femi_dev/how-to-use-axios-with-react-5fom
 
 function ProgramList() {
+
+  const [programs, setPrograms] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/programs").then((data) => {
+      console.log(data);
+      setPrograms(data?.data);
+    });
+  }, []);
+
+  // programs.map((anObjectMapped, index) => {
+  //   return (
+  //     <p key={`${anObjectMapped.name}_{anObjectMapped.email}`}>
+  //       {anObjectMapped.name} - {anObjectMapped.email}
+  //     </p>
+  //   );
+  // })
+
   return (
     <Card style={{ width: '18rem' }}>
       <Card.Img variant="top" src={benchPress} />
--- react-frontend/src/pages/Home.js
+++ react-frontend/src/pages/Home.js
@@ -1,6 +1,7 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 
 import ProgramList from '../components/ProgramsList'
 
@@ -11,6 +12,9 @@ function Home() {
       <Container>
         <Row>
           <Col><h1>Table of current Programs available</h1></Col>
+        </Row>
+        <Row>
+          <Col><a href="#" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Create a Program</a></Col>
         </Row>
         <Row>
           <Col><ProgramList /></Col>
