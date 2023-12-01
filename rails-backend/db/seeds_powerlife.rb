a034af20c29bbc284756f2a1f0257e3de72d9585
Justin HB <jbruell94@gmail.com>
2023-12-01 13:29:48 -0800

    Added Seed Files

--- rails-backend/db/seeds_powerlife.rb
+++ rails-backend/db/seeds_powerlife.rb
@@ -1,42 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
-# Create User Accounts
-User.first_or_create([
-  {
-    email: 'justin@lift.com',
-    password: 'password',
-    password_confirmation: 'password',
-  }
-])
-
-Dir[File.join(Rails.root, 'db', 'seeds/*', '*.rb')].sort.each do |seed|
-  load seed
-end
 
 programs = Program.create([
   {
-    name: "MAPS Performance",
-    description: "Designed to improve athletic performance, functional mobility, and a cultivate a balanced looking body"
-  },
-  # {
-  #   name: "MAPS Prime",
-  #   description: "Designed to provide supurior muscle recutment pattern, proper join mobility, prime the cental nevous system to maximize any muscle building signals"
-  # },
-  # {
-  #   name: "MAPS Anabolic",
-  #   description: "Designed to maximize muscle building and metabolism boosting potential."
-  # },
-  # {
-  #   name: "MAPS Asthetic",
-  #   description: "Designed to maximize muscle building and body sculpting potential"
-  # }
-])
+    name: "MAPS Powerlift",
+    description: "Designed to maximize your lifts in the three lifts; The Squat, The Bench Press, and the Deadlift."
+  }])
 
 
 phases = Phase.create([
